b7bfe71fe95707fcf6fcc98a64748e75be018e90
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2023-10-12 19:46:24 +0530

    commit 1 on 12 10 2023

---
+++ script.js
@@ -0,0 +1,5 @@
+var heroContent1 = $("#heroContent1");
+var heroContent2 = $("#heroContent2");
+heroContent1.on(':hover') = function(){
+    heroContent2.classList = heroContent2.classList + 'd-block';
+}
