db46367d0eb1ae65ddade02cb3f874183fbf5793
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2023-10-20 22:58:30 +0530

    commit 11 on 20 10 2023

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-var heroContent1 = $("#heroContent1");
-var heroContent2 = $("#heroContent2");
-heroContent1.on(':hover') = function(){
-    heroContent2.classList = heroContent2.classList + 'd-block';
-}
+var loadingDiv = document.getElementById('loadingDiv');
+document.onload = loadingDiv.style.display = 'none';
