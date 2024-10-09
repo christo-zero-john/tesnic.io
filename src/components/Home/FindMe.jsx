8fab6d2f62c7453363df2329dbdf1e87d55e75e6
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-09 22:30:29 +0530

    Refactored Home page code

---
+++ src/components/Home/FindMe.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function FindMe() {
+  return (
+    <div className="FindMe p-3">
+      <p className="h4 text-center text-secondary">Find Me at :</p>
+
+      <div className="FindMeItems text-center wd-90">
+        <a
+          className="my-md-4 mx-1  btn btn-outline-info GitHub"
+          href="https://github.com/christo-zero-john"
+        ></a>
+
+        <a
+          className="my-md-4 mx-1  btn btn-outline-info MuLearn"
+          href="https://app.mulearn.org/profile/christojohn-1@mulearn"
+        ></a>
+
+        <a
+          className="my-md-4 mx-1  btn btn-outline-info LinkedIn"
+          href="https://www.linkedin.com/in/christo-john-kalapurackal/"
+        ></a>
+
+        <a
+          className="my-md-4 mx-1  btn btn-outline-info Instagram"
+          href="https://www.instagram.com/tesnic.io"
+        ></a>
+
+        <a
+          className="my-md-4 mx-1  btn btn-outline-info facebook"
+          href="https://www.facebook.com/christojohn.kalapurackal"
+        ></a>
+
+        <a
+          className="my-md-4 mx-1  btn btn-outline-info w3Schools"
+          href="https://www.w3profile.com/tesnic"
+        ></a>
+      </div>
+    </div>
+  );
+}
+
+export default FindMe;
