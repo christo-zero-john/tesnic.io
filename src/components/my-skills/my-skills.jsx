b0ffc0188547ae67bbfeb710f55ec0847cb2e2c1
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-10 18:41:46 +0530

    Found new file,  src/components/my-skills/my-skills.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/my-skills/my-skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MySkills() {
+    let mySkills = {
+        frontend: ["HTML", "CSS", "JavaScript", "Bootstrap", "React"];
+        backend: ["Firebase"],
+        other: ["GitHub", "Project Management", "Communication"],
+    }
+    return (
+        <div>MySkills</div>
+    )
+}
+
+export default MySkills
