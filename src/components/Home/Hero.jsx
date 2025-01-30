13ffbd6e2cd5ed4f3fbd267aca35552cff5513bc
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-30 09:42:03 +0530

    Found modified or deleted file,  src/components/Home/Hero.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,23 +1,33 @@
+import SplitText from "../../lib/react-bits-animation-library/TextAnimations/SplitText/SplitText";
+
 function Hero() {
+  let delayStart = 100;
   return (
     <div className=" hero d-flex flex-row justify-content-center align-items-center">
       <div className="heroContent d-flex flex-column justify-content-around align-items-center heroBg">
-        <span
-          className="d-none d-md-block heroBrandImgLg img-fluid mb-5"
-        ></span>
-
-        <span
-          alt="Tesnic.io Brand Image"
-          className="d-md-none heroBrandImgSm img-fluid mb-5"
-        ></span>
-
-        <div className=" h2 text-center px-1 HeroText ">
-          Heyya! It's me
-          <div className="heroSpecial h1 text-center text-warning en-sirin">
-            <span className="heroTesnic ">TesNic </span>
-            <span className="HeroChristo d-none">Christo John </span>
+        <div className="display-1 text-center px-1 HeroText my-3">
+          <SplitText
+            delay={delayStart}
+            inDisplay={true}
+            text="Heyya! It's me"
+          />
+          <div className="heroSpecial text-center text-warning en-sirin">
+            <span className="heroTesnic ">
+              <SplitText
+                delay={delayStart + 100}
+                inDisplay={true}
+                text="TesNic"
+              />{" "}
+            </span>
+            <span className="HeroChristo d-none">
+              <SplitText
+                delay={delayStart + 150}
+                inDisplay={true}
+                text="Christo John"
+              />{" "}
+            </span>
           </div>
-          here
+          <SplitText delay={delayStart + 200} inDisplay={true} text="here" />
         </div>
         <p className=" web-dev btn nav-link text-capitalize btn-outline-success px-3 en-sirin">
           WEB-DEV
